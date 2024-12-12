d064b6b200b68b95347abadd7de8cd0dfb44f01a
Marcelo Finamor Vieira <marcelofinamorvieira@gmail.com>
2024-12-12 12:24:57 +0100

    vite

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,12 @@ import {
   IntentCtx,
   RenderFieldExtensionCtx,
   connect,
-} from "datocms-plugin-sdk";
-import { render } from "./utils/render";
-import ConfigScreen from "./entrypoints/ConfigScreen";
-import "datocms-react-ui/styles.css";
-import SlugExtension from "./entrypoints/SlugExtension";
-import updateAllChildrenSlugs from "./utils/updateAllChildrenSlugs";
+} from 'datocms-plugin-sdk';
+import { render } from './utils/render';
+import ConfigScreen from './entrypoints/ConfigScreen';
+import 'datocms-react-ui/styles.css';
+import SlugExtension from './entrypoints/SlugExtension';
+import updateAllChildrenSlugs from './utils/updateAllChildrenSlugs';
 
 connect({
   renderConfigScreen(ctx) {
@@ -16,16 +16,16 @@ connect({
   manualFieldExtensions(ctx: IntentCtx) {
     return [
       {
-        id: "treeLikeSlugs",
-        name: "Tree-like slugs",
-        type: "addon",
-        fieldTypes: ["slug"],
+        id: 'treeLikeSlugs',
+        name: 'Tree-like slugs',
+        type: 'addon',
+        fieldTypes: ['slug'],
       },
     ];
   },
   renderFieldExtension(fieldExtensionId: string, ctx: RenderFieldExtensionCtx) {
     switch (fieldExtensionId) {
-      case "treeLikeSlugs":
+      case 'treeLikeSlugs':
         return render(<SlugExtension ctx={ctx} />);
     }
   },
